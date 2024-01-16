33dcccbe5b461f6b8e12f3d6a930bddda1768396
HubedID <46119048+hubedid@users.noreply.github.com>
2024-01-16 13:05:31 +0700

    mod: exception

--- app/Config/Exceptions.php
+++ app/Config/Exceptions.php
@@ -81,28 +81,4 @@ class Exceptions extends BaseConfig
      * to capture logging the deprecations.
      */
     public string $deprecationLogLevel = LogLevel::WARNING;
-
-    /*
-     * DEFINE THE HANDLERS USED
-     * --------------------------------------------------------------------------
-     * Given the HTTP status code, returns exception handler that
-     * should be used to deal with this error. By default, it will run CodeIgniter's
-     * default handler and display the error information in the expected format
-     * for CLI, HTTP, or AJAX requests, as determined by is_cli() and the expected
-     * response format.
-     *
-     * Custom handlers can be returned if you want to handle one or more specific
-     * error codes yourself like:
-     *
-     *      if (in_array($statusCode, [400, 404, 500])) {
-     *          return new \App\Libraries\MyExceptionHandler();
-     *      }
-     *      if ($exception instanceOf PageNotFoundException) {
-     *          return new \App\Libraries\MyExceptionHandler();
-     *      }
-     */
-    public function handler(int $statusCode, Throwable $exception): ExceptionHandlerInterface
-    {
-        return new ExceptionHandler($this);
-    }
 }
